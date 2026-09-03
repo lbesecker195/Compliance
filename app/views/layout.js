01527f0e5c548d68a14b18b29c96c6fc55382350
Logan <logan@Logans-MacBook-Air.local>
2026-09-03 10:45:20 -0700

    Added G Analytics to layout.js.

--- app/views/layout.js
+++ app/views/layout.js
@@ -8,6 +8,15 @@ module.exports = function layout({ title, body, baseUrl = '', chrome }) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>${title}</title>
   <link rel="stylesheet" href="${baseUrl}/public/styles.css">
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-TJDDRBWMC5"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-TJDDRBWMC5');
+  </script>
 </head>
 <body class="${chrome ? 'reader-body' : ''}">
 ${body}
